b611f7b4b42cea27203cbff53feec37ac59a176a
SAKKU-LAPTOP\hydes <kws1044@gmail.com>
2021-08-09 19:30:37 +0900

    project init

---
+++ src/test/java/com/studyolleh/AppTests.java
@@ -0,0 +1,13 @@
+package com.studyolleh;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
