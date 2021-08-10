771c33df5b3effeb05558590271a7fb525955218
SAKKU-LAPTOP\hydes <kws1044@gmail.com>
2021-08-10 18:41:52 +0900

    signUpForm Validation

--- src/main/java/com/studyolleh/controller/AccountController.java
+++ src/main/java/com/studyolleh/controller/AccountController.java
@@ -1,20 +1,48 @@
 package com.studyolleh.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.studyolleh.account.SignUpForm;
+import com.studyolleh.account.SignUpFormValidator;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @Controller
 public class AccountController {
 
+	private final SignUpFormValidator signUpFormValidator;
+	
+	
+	@InitBinder("signUpForm")
+	public void initBinder(WebDataBinder webDataBinder) {
+		webDataBinder.addValidators(signUpFormValidator);
+	}
+	
 	@GetMapping("/sign-up")
-	public String signUpFrom(Model model) {
+	public String signUpForm(Model model) {
 //		model.addAttribute("signUpForm", new SignUpForm());
 		model.addAttribute(new SignUpForm());
 		return "account/sign-up";
 	}
+	
+	@PostMapping("/sign-up")
+	public String signUpSubmit(@Valid SignUpForm signUpForm, Errors errors) {
+		if(errors.hasErrors()) {
+			return "account/sign-up";
+		}
+		// TODO 회원 가입 처리
+		return "redirect:/";
+	}
+	
 }
 
 
